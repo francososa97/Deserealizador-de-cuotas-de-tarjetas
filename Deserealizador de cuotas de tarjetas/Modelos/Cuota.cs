976335db8b26b9f373fb839a9d799099d9596251
francososa97 <franco.gonzalez.sosa@gmail.com>
2021-06-25 13:15:34 +0200

    Agregar archivos de proyecto.

---
+++ Deserealizador de cuotas de tarjetas/Modelos/Cuota.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Deserealizador_de_cuotas_de_tarjetas.Modelos
+{
+    public class Cuota
+    {
+        /// <summary>
+        /// Atributo que guarda la fecha donde se realizo el consumo
+        /// </summary>
+        public DateTime FechaConsumo { get; set; }
+        /// <summary>
+        /// Elemento que contiene el elemento nombre de la cuota o movimiento
+        /// </summary>
+        public string Nombre { get; set; }
+        /// <summary>
+        /// Elemento que contiene el numero de cuotas pendientes que debes 
+        /// </summary>
+        public int CuotaPendiente { get; set; }
+        /// <summary>
+        /// Elemento que contiene el numero total de cuotas que tiene el movimeinto
+        /// </summary>
+        public int CuotaTotal { get; set; }
+        /// <summary>
+        /// Elemento que contiene el valor decimal del valor de la cuota
+        /// </summary>
+        public decimal ValorCuota { get; set; }
+        /// <summary>
+        /// Elemento que contiene el valor restante de la cuota
+        /// </summary>
+        public decimal TotalRestante { get; set; }
+
+        /// <summary>
+        /// constructor de la clase Cuota
+        /// </summary>
+        /// <param name="FechaConsumo"></param>
+        /// <param name="Nombre"></param>
+        /// <param name="CuotaPendiente"></param>
+        /// <param name="CuotaTotal"></param>
+        /// <param name="ValorCuota"></param>
+        public Cuota(string FechaConsumo, string Nombre,string CuotaPendiente, string CuotaTotal, string ValorCuota)
+        {
+            this.FechaConsumo = Convert.ToDateTime(FechaConsumo);
+            this.Nombre = Nombre;
+            this.CuotaTotal = Convert.ToInt32(CuotaTotal);
+            this.CuotaPendiente = Convert.ToInt32(CuotaPendiente);
+            this.ValorCuota = Convert.ToDecimal(ValorCuota);
+            this.TotalRestante = this.CuotaPendiente * this.ValorCuota;
+        }
+
+    }
+    /// <summary>
+    /// Enumerable de tipo mes  en el cual podemos castear el numero del mes con su respectivo nombre
+    /// </summary>
+    public enum TipoMes
+    {
+        Enero = 1,
+        Febrero = 2,
+        Marzo = 3,
+        Abril = 4,
+        Mayo = 5,
+        Junio = 6,
+        Julio = 7,
+        Agosto = 8,
+        Septiembre = 9,
+        Octubre = 10,
+        Noviembre = 11,
+        Diciembre = 12
+    }
+}
---
+++ Deserealizador de cuotas de tarjetas/Program.cs
@@ -0,0 +1,177 @@
+﻿using Deserealizador_de_cuotas_de_tarjetas.Modelos;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Deserealizador_de_cuotas_de_tarjetas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //cambiar la ruta depende querramos el archivo
+            string rutaArchivo = "../../../txt/Plan-cuotas.txt"; //en este guardamos el texto
+            string rutaArchivoResultante = "../../../txt/POST-Plan-Cuotas.txt"; //este archivo se crea con lo cargado en el anterior texto
+            string resultadoFS = GetCuotas(rutaArchivo);
+            var cuotas = CleanString(resultadoFS);
+            var resumenListo = BuildCuotas(cuotas);
+            var CuotasGrabar = ObtenerResumenes(resumenListo);
+            bool operacionExitosa = GrabarArchivo(CuotasGrabar, rutaArchivoResultante);
+
+            string respuesta = operacionExitosa ? "El archivo se genero correctamente" : "ocurrio un error al grabar el archivo";
+            Console.WriteLine(respuesta);
+        }
+
+        /// <summary>
+        /// Crear el modelo de cuotas con el array de string obtenido desde el archivo de entrada
+        /// </summary>
+        /// <param name="cuotas"></param>
+        /// <returns></returns>
+        public static List<Cuota> BuildCuotas(string[] cuotas)
+        {
+            List<Cuota> resumen = new List<Cuota>();
+            cuotas.ToList().ForEach(cuota =>
+            {
+                var cuotaclean = cuota.Split(" ");
+                Cuota newcuota = new Cuota(cuotaclean[0], cuotaclean[1], cuotaclean[2], cuotaclean[3], cuotaclean[4]);
+                resumen.Add(newcuota);
+            });
+            return resumen;
+        }
+
+        /// <summary>
+        /// Obteiene el mes actual de tu resumen haciendo una validacion previa
+        /// </summary>
+        /// <param name="tipoMes"></param>
+        /// <param name="mes"></param>
+        /// <returns></returns>
+        public static TipoMes ObtenerMes(TipoMes tipoMes, int mes)
+        {
+            int ValorMes = (int)tipoMes;
+            if ((ValorMes + mes) > 12)
+            {
+                int mesActual = (ValorMes + mes) - 12;
+                return (TipoMes)mesActual;
+            }
+            else
+            {
+                int mesActual = ValorMes + mes;
+                return (TipoMes)mesActual;
+
+            }
+        }
+
+        /// <summary>
+        /// Metodo que crear el string a grabar en el archivo txt, recibiendo el modelo de resumen
+        /// </summary>
+        /// <param name="resumen"></param>
+        /// <returns></returns>
+        public static StringBuilder ObtenerResumenes(List<Cuota> resumen)
+        {
+            StringBuilder registro = new StringBuilder();
+            int cuotaMayorPlazo = resumen.Max(x => x.CuotaPendiente);
+            int indice = 0;
+            TipoMes mesActual = (TipoMes)DateTime.Now.Month;
+
+            while (cuotaMayorPlazo > 0)
+            {
+                MostrarCuotasMensuales(registro, resumen, mesActual, indice);
+
+                cuotaMayorPlazo--;
+                indice++;
+            }
+
+            return registro;
+
+        }
+
+        /// <summary>
+        /// Graba las cuotas mensuales en el string builder
+        /// </summary>
+        /// <param name="registro"></param>
+        /// <param name="cuotas"></param>
+        /// <param name="mesActual"></param>
+        /// <param name="indice"></param>
+        /// <param name="marca"></param>
+        public static void MostrarCuotasMensuales(StringBuilder registro, List<Cuota> cuotas, TipoMes mesActual, int indice)
+        {
+            List<decimal> TotalResumen = new List<decimal>();
+            registro.AppendLine($"-------------------- Resumen de mes {ObtenerMes(mesActual, indice)} --------------------\n Nombre   |   Cuota actual    |   Cuota Total |   valor cuota | restante \n");
+
+            cuotas.ForEach(cuota =>
+            {
+                int pocicionCuota = cuota.CuotaPendiente - indice;
+                if (pocicionCuota > 0)
+                {
+                    TotalResumen.Add(cuota.ValorCuota);
+                    string comentarioAddicional = pocicionCuota == 1 ? "ULTIMA CUOTA -->" : null;
+                    string registroCuota = $" {comentarioAddicional} {cuota.FechaConsumo.ToShortDateString()}    |   {cuota.Nombre}  |   {pocicionCuota} |   {cuota.CuotaTotal}  |   {cuota.ValorCuota}  |   {pocicionCuota * cuota.ValorCuota} ";
+                    registro.AppendLine(registroCuota);
+
+                }
+            });
+            registro.AppendLine("\n");
+            registro.AppendLine($"Total mensual: {TotalResumen.Sum()}");
+        }
+
+        /// <summary>
+        /// Metodo que graba el archivo txt de tu resumen recibe el string
+        /// </summary>
+        /// <param name="stringBuilder"></param>
+        /// <returns></returns>
+        public static bool GrabarArchivo(StringBuilder stringBuilder,string ruta)
+        {
+            try
+            {
+                using (StreamWriter Archivo = new StreamWriter(ruta, true, Encoding.UTF8))
+                {
+                    Archivo.WriteLine(stringBuilder.ToString());
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+
+            }
+        }
+
+        /// <summary>
+        /// Pasamos el string obtenido del archivo y lo preparamos para ser mapeado con el modelo
+        /// </summary>
+        /// <param name="cuotas"></param>
+        /// <returns></returns>
+        public static string[] CleanString(string cuotas)
+        {
+            string cuotasClean = cuotas.Trim();
+            var cuotasConIones = cuotasClean.Replace("\t", "-");
+            string cuotasConEspacio = cuotasConIones.Replace("-", " ");
+            string[] cuotasIterables = cuotasConEspacio.Split('\n');
+            return cuotasIterables;
+        }
+
+        /// <summary>
+        /// Metodo que se encarga de obtener la informacion del archivo solicitado
+        /// </summary>
+        /// <param name="ruta"></param>
+        /// <returns></returns>
+        public static string GetCuotas(string ruta)
+        {
+            string cuotas = string.Empty;
+            try
+            {
+                cuotas = File.ReadAllText(ruta);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("verifique la ruta del archivo solicitado");
+                Console.WriteLine($"Error {e.Message}");
+                throw;
+            }
+            return cuotas;
+        }
+    }
+}
